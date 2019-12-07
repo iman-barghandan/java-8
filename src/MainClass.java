893c807285876e84ee037cd2bde6b73122020f3b
iman92dez <iman.barghandan1371@gmail.com>
2019-12-07 18:14:40 +0330

    use sortByLastName and get list

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,8 +1,11 @@
 import person.Person;
 import person.PersonObjects;
+import sort.ListOfLastName;
 import sort.SortByAge;
+import sort.SortByLengthLastName;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainClass
 {
@@ -15,6 +18,14 @@ public class MainClass
         ArrayList<Person> arrayListAge = SortByAge.execute(arrayListPerson);
         arrayListAge.forEach(System.out::println);
 //        arrayListAge.forEach(c -> System.out.println(c));
+        System.out.println("**********************************");
+
+        System.out.println("sort by Lengths of LastName: ");
+        //sort by LastName(Lengts):
+        ArrayList<Person> arrayListLastName = SortByLengthLastName.execute(arrayListPerson);
+        //get list of LastName:
+        List<String> stringList = ListOfLastName.execute(arrayListLastName);
+        stringList.forEach(System.out::println);
         System.out.println("**********************************");
 
 
