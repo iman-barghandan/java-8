859ecbcc629c35d81c07d7a9d79fa91ca49d5c58
iman92dez <iman.barghandan1371@gmail.com>
2019-12-07 18:12:32 +0330

    create MainClass use SortByAge

---
+++ src/MainClass.java
@@ -0,0 +1,22 @@
+import person.Person;
+import person.PersonObjects;
+import sort.SortByAge;
+
+import java.util.ArrayList;
+
+public class MainClass
+{
+    public static void main(String[] args) {
+        PersonObjects po = new PersonObjects();
+        ArrayList<Person> arrayListPerson = po.listOfPerson();
+
+        // sort by "age":
+        System.out.println("sort by age:");
+        ArrayList<Person> arrayListAge = SortByAge.execute(arrayListPerson);
+        arrayListAge.forEach(System.out::println);
+//        arrayListAge.forEach(c -> System.out.println(c));
+        System.out.println("**********************************");
+
+
+    }
+}
